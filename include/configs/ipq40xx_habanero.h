ec953191981ebe8fb68e9fe9d7b040be6a235210
Robert Marko <robert.marko@sartura.hr>
2020-12-02 18:02:52 +0100

    habanero: update default partitions
    mtdparts need to be updated
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- include/configs/ipq40xx_habanero.h
+++ include/configs/ipq40xx_habanero.h
@@ -35,7 +35,7 @@
 #define CONFIG_CMD_UBI
 #endif
 
-#define MTDPARTS_DEFAULT	"mtdparts=nand2:0x1E40000@0x1C0000(firmware)"
+#define MTDPARTS_DEFAULT	"mtdparts=nand2:0x1E40000@0x1C0000(firmware);nand0:128m(ubi1),128m(ubi2)"
 #define MTDIDS_DEFAULT		"nand0=nand0,nand2=nand2"
 
 #define CONFIG_FACTORY_IMG_FILENAME    "habanero.bin"
